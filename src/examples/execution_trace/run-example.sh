1c0ea89a25c5b63b7180baad58fb4df95b11870c
Yanlong Yin <yin@hawk.iit.edu>
2013-11-11 17:32:09 -0600

    Improved example execution_trace according to the current collector.

--- src/examples/execution_trace/run-example.sh
+++ src/examples/execution_trace/run-example.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+rm -f ./exe*.out
+rm -f ./mpiio*.out
+rm -f ./posix*.out
+rm -f ./exe*.out.new
 
 # build the executable
 make clean
-make
+make -f Makefile.iosig
 
 # run the executable, this step also generates the 
 # execution tracing file, named exe_pid.out
@@ -11,5 +15,5 @@ make
 # use the script to translate the trace to readable format
 for trace in $(ls exe*.out)
 do
-	source ../../trace_collect/readexelog.sh ./main $trace 2>&1 | tee $trace.new
+	source ../../collect/readexelog.sh ./main $trace 2>&1 >> $trace.new
 done
