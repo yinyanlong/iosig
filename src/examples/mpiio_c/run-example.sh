5a5ea84d1fb4a73d5e4a5b087a27d8bfe41f273d
Yanlong Yin <yin@hawk.iit.edu>
2013-11-11 17:34:57 -0600

    Improved example mpiio_c.

--- src/examples/mpiio_c/run-example.sh
+++ src/examples/mpiio_c/run-example.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-rm -f ./trace*.out
 rm -f ./exe*.out
 rm -f ./mpiio*.out
 rm -f ./posix*.out
 rm -f ./exe*.out.new
 
 make clean
-make
+make -f Makefile.iosig
 mpiexec -np 2 ./example ./example.test.data 2>&1 | tee strace.log
 
 # use the script to translate the trace to readable format
 for trace in $(ls exe*.out)
 do
-	source ../../collect2/readexelog.sh ./example $trace 2>&1 >> $trace.new
+	source ../../collect/readexelog.sh ./example $trace 2>&1 >> $trace.new
 done
 
