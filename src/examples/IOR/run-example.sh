358c70fc25e56c19b469acd957c8c4bb789004fb
Yanlong Yin <yinyanlong@gmail.com>
2014-02-28 14:40:54 -0600

    Improved Makefile of IOR example.

--- src/examples/IOR/run-example.sh
+++ src/examples/IOR/run-example.sh
@@ -5,13 +5,8 @@ rm -f ./mpiio*.out
 rm -f ./posix*.out
 rm -f ./exe*.out.new
 
-make clean
-make
+#make clean
+#make
 
-mpiexec -np 1 ./IOR -a MPIIO -b 24m -o /tmp/datafile -w -k -t 4k
+gdb --args mpiexec -np 1 ./IOR -a MPIIO -b 24m -o /tmp/datafile -w -k -t 4k
 
-# use the script to translate the trace to readable format
-for trace in $(ls exe*.out)
-do
-	source ../../collect/readexelog.sh ./IOR $trace 2>&1 >> $trace.new
-done
